e2759bec8badf8c82ceb6caad0cd53253fc74a86
NurettinKaynar <nurettinkaynar568@gmail.com>
2023-12-18 11:26:11 +0300

    başarısız deneme

---
+++ src/app/core/utils/apiUrl.ts
@@ -0,0 +1,6 @@
+export class ApiUrl {
+  public static readonly BASE_API_URL = "https://rickandmortyapi.com/api";
+  public static readonly GET_ALL_CHARACTERS = "/character";
+  public static readonly GET_ALL_EPISODE = "/episode";
+  public static readonly GET_ALL_LOCATION = "/location";
+}
